dbdc12095165aa2b301bc0726bc06ec7a00e5026
Bharat Shinde <129615281+Shinde-Bharat@users.noreply.github.com>
2024-02-08 23:38:36 +0530

    Add files via upload

---
+++ EMS/aloginwel.php
@@ -0,0 +1,61 @@
+<?php 
+require_once ('process/dbh.php');
+$sql = "SELECT id, firstName, lastName FROM employee order by salary desc";
+$result = mysqli_query($conn, $sql);
+?>
+
+
+<html>
+<head>
+	<title>Admin Panel | Infosys Corporation</title>
+	<link rel="stylesheet" type="text/css" href="styleemplogin.css">
+</head>
+<body>
+	
+	<header>
+		<nav>
+			<h1>Infosys Corp.</h1>
+			<ul id="navli">
+				<li><a class="homered" href="aloginwel.php">HOME</a></li>
+				<li><a class="homeblack" href="addemp.php">Add Employee</a></li>
+				<li><a class="homeblack" href="viewemp.php">View Employee</a></li>
+				<li><a class="homeblack" href="alogin.html">Log Out</a></li>
+			</ul>
+		</nav>
+	</header>
+	 
+	<div class="divider"></div>
+	<div id="divimg">
+		<h2 style="font-family: 'Montserrat', sans-serif; font-size: 25px; text-align: center;">Empolyee Leaderboard </h2>
+    	<table>
+
+			<tr bgcolor="#000">
+				<th align = "center">Seq.</th>
+				<th align = "center">Emp. ID</th>
+				<th align = "center">Name</th>
+				
+
+			</tr>
+
+			
+
+			<?php
+				$seq = 1;
+				while ($employee = mysqli_fetch_assoc($result)) {
+					echo "<tr>";
+					echo "<td>".$seq."</td>";
+					echo "<td>".$employee['id']."</td>";
+					
+					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
+					
+					$seq+=1;
+				}
+
+
+			?>
+
+		</table>
+		
+	</div>
+</body>
+</html>
